c454267c78c87fd94cc54d631e4a8a322a2f93a9
mrsashemi <mrsashemi@gmail.com>
2022-06-29 01:23:04 -0700

    Create todo object constructor and html form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 import './style.css'
+
+//Create a to-do list array to hold the todo objects
+let myToDoList = [];
+
+//Create a factory function that generates todo objects
+const toDo = function(title, notes, date, project, completed, remind, priority ) {
+    return {
+        title,
+        notes,
+        date,
+        project,
+        completed,
+        remind,
+        priority
+    }
+}
